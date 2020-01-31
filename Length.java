e76c0e173a331a376a9f8b429a905c031d5e515d
janoia <jordano.anoia@hotmail.ca>
2020-01-31 14:13:58 -0500

    added Length

---
+++ Length.java
@@ -0,0 +1,10 @@
+public class Length {
+  public static void main(String[] args) {
+  	int Cent;
+  	double feet;
+  	Cent = 60;
+  	feet = Cent/30.48;
+
+  	System.out.println ("There are:" + Cent + "cm which equals to:" + feet + "ft");
+  }
+}
