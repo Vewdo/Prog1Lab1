0712eae19a16b96c0e35c0916ab06ba15b985097
janoia <jordano.anoia@hotmail.ca>
2020-01-31 13:27:16 -0500

    added MyAgeIs

---
+++ MyAgeIs.java
@@ -0,0 +1,12 @@
+public class MyAgeIs {
+  public static void main(String[] args) {
+  
+  int currentYear;
+  int YearOfBirth;
+  int MyAge;
+  currentYear = 2020;
+  YearOfBirth = 2001;
+  MyAge = currentYear - YearOfBirth;
+  System.out.println (" My Age Is" + MyAge);
+ }
+}
