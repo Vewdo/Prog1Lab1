a47c0da01947c0aa17a344d40bc0573d62c85051
janoia <jordano.anoia@hotmail.ca>
2020-02-03 09:31:35 -0500

    added workbook exercises

---
+++ AreaOfCircle.java
@@ -0,0 +1,12 @@
+public class AreaOfCircle {
+
+    public static void main(String[] args) {
+        final double PI = 3.14159;
+
+        double radius = 3.5;
+
+        double area = PI * radius * radius;
+
+        System.out.println("The area of the Circle is " + area);
+    }
+}
---
+++ FirstProgram.java
@@ -0,0 +1,9 @@
+public class FirstProgram {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Programming is not a spectator sport!");
+        System.exit (0);
+    }
+
+}
